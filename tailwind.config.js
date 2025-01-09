4608d442e075cd255a30470e344851aaf70c68b7
isrraelantonio <israelantonio027@gmail.com>
2025-01-09 15:56:07 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+
+        /**Também podemos customizar o nosso site com o tailwind e definir pessos para essa cor específica */
+        clifford: {
+
+          100: '#32a852',
+          200: '#146e2c',
+          300: '#053311',
+          
+      }
+    }
+  }
+}
+}
