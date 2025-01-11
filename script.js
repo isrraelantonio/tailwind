7ad754ea4d89ad7cf6dbfefcb8f73cd050fd5d2b
isrraelantonio <israelantonio027@gmail.com>
2025-01-11 15:16:40 -0300

    criação de um menu responsivo e inserção de ícones no nosso foother

---
+++ script.js
@@ -0,0 +1,19 @@
+/*function retiramenu(){
+
+      var fundo =  document.querySelector('#menu');
+
+    if(fundo.style.display = "hidden") {
+        fundo.style.display = "flex"
+    }
+    if(fundo.style.display = "flex") {
+        fundo.style.display = "hidden"
+    }
+
+
+}*/
+
+function dropdown(){
+    document.querySelector('.menu').classList.toggle("menu2")
+    
+   
+ }
